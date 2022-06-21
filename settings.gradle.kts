b9b1ce2495f30b6313bea0e11352a736deb66a81
Эдуард <edurd@rambler.ru>
2022-06-21 20:37:00 +0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.edurda77.transfer-backend"
---
+++ src/main/kotlin/com/edurda77/Application.kt
@@ -0,0 +1,11 @@
+package com.edurda77
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.edurda77.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "192.168.1.105") {
+        configureRouting()
+    }.start(wait = true)
+}
