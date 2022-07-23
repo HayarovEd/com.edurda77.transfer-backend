d6c5d1358e4e0604869fc25767522526eeff36bb
hayarovy <edurd@rambler.ru>
2022-07-23 13:55:34 +0500

    add correct data

--- src/main/kotlin/com/edurda77/database/UserDb.kt
+++ src/main/kotlin/com/edurda77/database/UserDb.kt
@@ -3,13 +3,17 @@ package com.edurda77.database
 import com.edurda77.model.DbModel
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.text.SimpleDateFormat
+import java.util.*
 
 object UserDb : Table("users") {
     private val login = varchar("login", 20)
     private val password = varchar("password", 20)
     private val lastData = integer("lastdata")
     private val currentData = integer("currentdata")
-//ver
+    private val updateDate = varchar("dataofupdate", 15)
+
+    //ver
     fun updateData(dbModel: DbModel, currentData: Int) {
         transaction {
             removeUser(dbModel)
@@ -17,7 +21,8 @@ object UserDb : Table("users") {
                 login = dbModel.login,
                 password = dbModel.password,
                 lastData = dbModel.currentData,
-                currentData = currentData
+                currentData = currentData,
+                updateDate = dateToString()
             )
             addUser(tempUser)
         }
@@ -30,7 +35,8 @@ object UserDb : Table("users") {
             }
         }
     }
-//ver
+
+    //ver
     fun addUser(dbModel: DbModel) {
         transaction {
             UserDb.insert {
@@ -38,10 +44,12 @@ object UserDb : Table("users") {
                 it[password] = dbModel.password
                 it[lastData] = dbModel.lastData
                 it[currentData] = dbModel.currentData
+                it[updateDate] = dateToString()
             }
         }
     }
-// ver
+
+    // ver
     fun changePassword(dbModel: DbModel, passwordUser: String) {
         transaction {
             removeUser(dbModel)
@@ -49,12 +57,14 @@ object UserDb : Table("users") {
                 login = dbModel.login,
                 password = passwordUser,
                 lastData = dbModel.lastData,
-                currentData = dbModel.currentData
+                currentData = dbModel.currentData,
+                updateDate = dateToString()
             )
             addUser(tempUser)
         }
     }
-//ver
+
+    //ver
     fun fetchUser(login: String): DbModel? {
         return try {
             transaction {
@@ -63,14 +73,16 @@ object UserDb : Table("users") {
                     login = userModel[UserDb.login],
                     password = userModel[password],
                     lastData = userModel[lastData],
-                    currentData = userModel[currentData]
+                    currentData = userModel[currentData],
+                    updateDate = dateToString()
                 )
             }
         } catch (e: Exception) {
             null
         }
     }
-// ver
+
+    // ver
     fun fetchUsers(): List<DbModel> {
         val users: MutableList<DbModel> = mutableListOf()
         transaction {
@@ -81,7 +93,8 @@ object UserDb : Table("users") {
                         login = it[login],
                         password = it[password],
                         lastData = it[lastData],
-                        currentData = it[currentData]
+                        currentData = it[currentData],
+                        updateDate = dateToString()
                     )
                 )
             }
@@ -89,14 +102,23 @@ object UserDb : Table("users") {
         }
         return users
     }
+
     fun correctData(dbModel: DbModel, newLastData: Int, newCurrentData: Int) {
         removeUser(dbModel)
-        addUser(DbModel(
-            login = dbModel.login,
-            password = dbModel.password,
-            lastData = newLastData,
-            currentData = newCurrentData
-        ))
+        addUser(
+            DbModel(
+                login = dbModel.login,
+                password = dbModel.password,
+                lastData = newLastData,
+                currentData = newCurrentData,
+                updateDate = dateToString()
+            )
+        )
     }
 
+    private fun dateToString(): String {
+        val date = Date()
+        val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        return sdf.format(date)
+    }
 }
